58e55a49c27fa1eba3fba6f90d2ba3de487f8cfe
ST Chiew <st.chiew@gmail.com>
2020-02-29 15:26:53 +0800

    added swiper

--- src/webparts/pageSwiper/components/PageCard/PageCard.tsx
+++ src/webparts/pageSwiper/components/PageCard/PageCard.tsx
@@ -1,7 +1,9 @@
 import * as React from 'react';
 import IPageCardProps from './IPageCardProps';
+import styles from './PageCard.module.scss';
 
-
-const PageCard = (item: IPageCardProps) => <h3>{item.pageItem.Title}</h3>;
+const PageCard = (item: IPageCardProps) => {
+  return (<div className={styles.container}>{item.pageItem.Title}</div>);
+};
 
 export default PageCard;
--- src/webparts/pageSwiper/components/PageSwiper.tsx
+++ src/webparts/pageSwiper/components/PageSwiper.tsx
@@ -3,8 +3,18 @@ import styles from './PageSwiper.module.scss';
 import { IPageSwiperProps } from './IPageSwiperProps';
 import { IPageItem } from '../services/IPageItem';
 import PageCard from './PageCard/PageCard';
+import Swiper from 'react-id-swiper';
+import 'swiper/css/swiper.min.css';
 
 export default function PageSwiper(props: IPageSwiperProps) {
+  const params = {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    }
+  };
   const initialPages: IPageItem[] = null;
   const [pages, setPages] = React.useState(initialPages);
   var content: any = null;
@@ -17,17 +27,13 @@ export default function PageSwiper(props: IPageSwiperProps) {
   );
 
   if (pages == null) {
-    content = (<div></div>);
+    content = null;
   } else {
-    content = (<div>{pages.map((page, i) => <div> <PageCard pageItem={page} key={i} /></div>)}</div>);
+    content = (<Swiper {...params}>{pages.map((page, i) => <div> <PageCard pageItem={page} key={i} /></div>)}</Swiper>);
   }
   return (
     <div className={styles.pageSwiper}>
-      <div className={styles.container}>
-        <div className={styles.row}>
-          {content}
-        </div>
-      </div>
+      {content}
     </div>
   );
 
